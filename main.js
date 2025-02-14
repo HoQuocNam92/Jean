842f98f497f6d05ef1b42313470cd2761fcd901f
hoquocnam92 <hoquocnam92@gmail.com>
2025-02-14 18:00:46 +0700

    FIX DONE

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const Jeans = document.querySelector('.link-jeans');
 const Menu = document.querySelector('.sub-menu');
 const MenuLink = document.querySelector('.selected');
-console.log(MenuLink)
+const Bars = document.querySelector('.icons');
+const SideBar = document.querySelector('#nav');
+const overlay = document.querySelector('#overlay');
+const closeBars  = document.querySelector('.close');
 Jeans.addEventListener('click', function(e){
     Menu.classList.toggle('show'); 
     MenuLink.classList.toggle('active');
     e.preventDefault(); // prevent default link behavior to open in new tab
 })
+
+Bars.addEventListener('click', function(){
+    SideBar.classList.toggle('navbars'); 
+    overlay.style.display = 'block';
+    
+});
+overlay.addEventListener('click', function(){
+    overlay.style.display = 'none';
+    SideBar.classList.toggle('navbars'); 
+});
+closeBars.addEventListener('click', function(){
+    overlay.style.display = 'none';
+    SideBar.classList.toggle('navbars');
+});
